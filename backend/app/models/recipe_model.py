a11e2294abe78dd136bc5a67338e6d1ad4110d25
Sebastian Heynes <heynes.sebastian@gmail.com>
2026-03-26 20:31:25 +0100

    added full recipe CRUD and CORS support

--- backend/app/models/recipe_model.py
+++ backend/app/models/recipe_model.py
@@ -8,18 +8,31 @@ class RecipeIngredient(BaseModel):
     unit: Literal["kg", "g", "l", "dl", "ml", "msk", "tsk", "krm", "stk"]
 
 
+class RecipeStep(BaseModel):
+    step_number: int = Field(gt=0)
+    instruction: str = Field(min_length=1, max_length=200)
+
+
+CategoryType = Literal[
+    "breakfast", "lunch", "dinner", "dessert", "meat", "fish", "vegetarian", "vegan"
+]
+
+
 class RecipeCreate(BaseModel):
     title: str = Field(min_length=1, max_length=100)
     cooking_duration: int = Field(gt=0)
     portion: int = Field(gt=0)
-    category: Literal[
-        "breakfast", "lunch", "dinner", "dessert", "meat", "fish", "vegetarian", "vegan"
-    ]
+    category: CategoryType
     ingredients: List[RecipeIngredient]
     recipe_steps: List[str]
     picture: Optional[str] = None
 
 
-class RecipeStep(BaseModel):
-    step_number: int = Field(gt=0)
-    instruction: str = Field(min_length=1, max_length=200)
+class RecipeUpdate(BaseModel):
+    title: Optional[str] = None
+    cooking_duration: Optional[int] = None
+    portion: Optional[int] = None
+    category: Optional[CategoryType] = None
+    ingredients: Optional[List[RecipeIngredient]] = None
+    recipe_steps: Optional[List[str]] = None
+    picture: Optional[str] = None
--- backend/app/routes/recipe_routes.py
+++ backend/app/routes/recipe_routes.py
@@ -5,6 +5,7 @@ from app.repositories.db_manager import get_connection
 recipe_bp = Blueprint("recipe_bp", __name__)
 
 
+# Route to create a recipe
 @recipe_bp.route("/", methods=["POST"])
 def post_recipe():
     data = request.get_json()
@@ -13,7 +14,104 @@ def post_recipe():
     try:
         db_connection = get_connection()
         result = recipe_service.add_new_recipe(db_connection, data)
-        return jsonify(result), 201
+        return (
+            jsonify(
+                {"message": f"Recipe with ID: {result} created", "id": result}
+            ),
+            201,
+        )
 
-    except ValueError as e:
+    except Exception as e:
         return jsonify({"error": str(e)}), 400
+
+    finally:
+        if db_connection:
+            db_connection.close()
+
+
+# Route to fetch all recipes
+@recipe_bp.route("/", methods=["GET"])
+def get_recipes():
+    db_connection = None
+
+    try:
+        db_connection = get_connection()
+        recipes = recipe_service.get_all_recipes(db_connection)
+        return jsonify(recipes), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+    finally:
+        if db_connection:
+            db_connection.close()
+
+
+# Route to fetch recipe by ID
+@recipe_bp.route("/id/<int:recipe_id>", methods=["GET"])
+def get_recipe_by_id(recipe_id):
+    db_connection = None
+
+    try:
+        db_connection = get_connection()
+        recipe = recipe_service.get_recipe_by_id(db_connection, recipe_id)
+        if recipe:
+            return jsonify(recipe), 200
+        else:
+            return jsonify({"error": "Not Found"}), 404
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+    finally:
+        if db_connection:
+            db_connection.close()
+
+
+# Route to update a specific recipe
+@recipe_bp.route("/id/<int:recipe_id>", methods=["PUT"])
+def update_recipe(recipe_id):
+    data = request.get_json()
+    db_connection = None
+
+    try:
+        db_connection = get_connection()
+        result = recipe_service.update_recipe(db_connection, recipe_id, data)
+
+        return (
+            jsonify(
+                {"message": f"Recipe with ID: {recipe_id} updated", "id": recipe_id}
+            ),
+            200,
+        )
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+    finally:
+        if db_connection:
+            db_connection.close()
+
+
+# Route to delete a specific recipe
+@recipe_bp.route("/id/<int:recipe_id>", methods=["DELETE"])
+def delete_recipe(recipe_id):
+    db_connection = None
+
+    try:
+        db_connection = get_connection()
+        result = recipe_service.delete_recipe(db_connection, recipe_id)
+
+        return (
+            jsonify(
+                {"message": f"Recipe with ID: {recipe_id} deleted", "id": recipe_id}
+            ),
+            200,
+        )
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+    finally:
+        if db_connection:
+            db_connection.close()
